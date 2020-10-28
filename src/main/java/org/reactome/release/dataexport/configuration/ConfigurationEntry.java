32165ab30f28215c045adc98a89f65efa30ee40a
Joel Weiser <joel.weiser@oicr.on.ca>
2020-10-28 00:49:05 -0400

    updates ConfigurationEntry to allow user input mocking in unit tests

--- src/main/java/org/reactome/release/dataexport/configuration/ConfigurationEntry.java
+++ src/main/java/org/reactome/release/dataexport/configuration/ConfigurationEntry.java
@@ -13,7 +13,7 @@ public abstract class ConfigurationEntry {
 	private String userInputValue;
 
 	/**
-	 * Creates an object for obtaining and representing a configuration entry
+	 * Creates an object for obtaining and representing a configuration entry.
 	 *
 	 * @param userPromptValueName The name or description to insert into a prompt to the user for a configuration value
 	 * @param configurationEntryName Name or key of the configuration entry (a value is obtained on request from a call
@@ -52,7 +52,7 @@ public abstract class ConfigurationEntry {
 	/**
 	 * Returns the full configuration entry as a String in the form of "key=value".  If the value hasn't been already
 	 * been obtained from user input via a prompt, the value will be request from the user (and cached for subsequent
-	 * calls to this method)
+	 * calls to this method).
 	 *
 	 * @return Configuration entry as a String in the form "key=value"
 	 */
@@ -71,16 +71,30 @@ public abstract class ConfigurationEntry {
  * @author jweiser
  */
 class RequiredConfigurationEntry extends ConfigurationEntry {
+	private Scanner scanner;
 
 	/**
-	 * Creates an object for obtaining and representing a configuration entry
+	 * Creates an object for obtaining and representing a configuration entry.
 	 *
 	 * @param userPrompt The name or description to insert into a prompt to the user for a configuration value
 	 * @param entryName Name or key of the configuration entry (a value is obtained on request from a call
 	 * to the getValueFromUserInput() method via a user prompt)
 	 */
 	public RequiredConfigurationEntry(String userPrompt, String entryName) {
+		this(userPrompt, entryName, new Scanner(System.in));
+	}
+
+	/**
+	 * Creates an object for obtaining and representing a configuration entry.
+	 *
+	 * @param userPrompt The name or description to insert into a prompt to the user for a configuration value
+	 * @param entryName Name or key of the configuration entry (a value is obtained on request from a call
+	 * to the getValueFromUserInput() method via a user prompt)
+	 * @param scanner Scanner object from which to retrieve input for the configuration value
+	 */
+	RequiredConfigurationEntry(String userPrompt, String entryName, Scanner scanner) {
 		super(userPrompt, entryName);
+		this.scanner = scanner;
 	}
 
 	/**
@@ -95,10 +109,14 @@ class RequiredConfigurationEntry extends ConfigurationEntry {
 		do {
 			System.out.print("Enter " + getUserPromptValueName() + ": ");
 
-			value = new Scanner(System.in).nextLine();
+			value = getScanner().nextLine();
 		} while(value == null || value.isEmpty());
 
 		return value;
+	}
+
+	private Scanner getScanner() {
+		return this.scanner;
 	}
 }
 
@@ -109,9 +127,10 @@ class RequiredConfigurationEntry extends ConfigurationEntry {
  */
 class OptionalConfigurationEntry extends ConfigurationEntry {
 	private String defaultValue;
+	private Scanner scanner;
 
 	/**
-	 * Creates an object for obtaining and representing a configuration entry
+	 * Creates an object for obtaining and representing a configuration entry.
 	 *
 	 * @param userPrompt The name or description to insert into a prompt to the user for a configuration value
 	 * @param entryName Name or key of the configuration entry (a value is obtained on request from a call to the
@@ -119,13 +138,27 @@ class OptionalConfigurationEntry extends ConfigurationEntry {
 	 * @param defaultValue Value used if no value is provided by the user
 	 */
 	public OptionalConfigurationEntry(String userPrompt, String entryName, String defaultValue) {
+		this(userPrompt, entryName, defaultValue, new Scanner(System.in));
+	}
+
+	/**
+	 * Creates an object for obtaining and representing a configuration entry.
+	 *
+	 * @param userPrompt The name or description to insert into a prompt to the user for a configuration value
+	 * @param entryName Name or key of the configuration entry (a value is obtained on request from a call to the
+	 * getValueFromUserInput() method via a user prompt)
+	 * @param defaultValue Value used if no value is provided by the user
+	 * @param scanner Scanner object from which to retrieve input for the configuration value
+	 */
+	OptionalConfigurationEntry(String userPrompt, String entryName, String defaultValue, Scanner scanner) {
 		super(userPrompt, entryName);
 		this.defaultValue = defaultValue;
+		this.scanner = scanner;
 	}
 
 	/**
 	 * Returns the value for the configuration entry.  The user is prompted for a value, but if no value is entered, a
-	 * default value provided upon instantiation of this class is used
+	 * default value provided upon instantiation of this class is used.
 	 *
 	 * @return Value for the configuration entry (either provided by the user or the default belonging to this object)
 	 */
@@ -133,7 +166,7 @@ class OptionalConfigurationEntry extends ConfigurationEntry {
 	protected String getValueFromUserInput() {
 		System.out.print("Enter " + getUserPromptValueName() + " (leave blank for " + getDefaultValue() + "): ");
 
-		String value = new Scanner(System.in).nextLine();
+		String value = getScanner().nextLine();
 		if (value == null || value.isEmpty()) {
 			value = getDefaultValue();
 		}
@@ -143,6 +176,10 @@ class OptionalConfigurationEntry extends ConfigurationEntry {
 
 	private String getDefaultValue() {
 		return this.defaultValue;
+	}
+
+	private Scanner getScanner() {
+		return this.scanner;
 	}
 }
 
@@ -189,6 +226,18 @@ class PasswordConfigurationEntry extends ConfigurationEntry {
 		return password;
 	}
 
+	/*
+	 * This method is package-private to enable mocking of password retrieval in unit tests, but still have the default
+	 * behaviour of reading the password from user input via the System console.
+	 *
+	 * @return Password read (by default from the user via the System console)
+	 */
+	String readPassword() {
+		Console console = System.console();
+		char[] password = console.readPassword();
+		return new String(password);
+	}
+
 	private String askUserForPasswordUntilReceived() {
 		return promptForPasswordUntilReceived("Enter " + getUserPromptValueName() + ": ");
 	}
@@ -207,8 +256,6 @@ class PasswordConfigurationEntry extends ConfigurationEntry {
 
 	private String promptForPassword(String prompt) {
 		System.out.print(prompt);
-		Console console = System.console();
-		char[] password = console.readPassword();
-		return new String(password);
+		return readPassword();
 	}
 }
