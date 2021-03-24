a44043c983864717682400db7f93a337147ed389
Joel Weiser <joel.weiser@oicr.on.ca>
2021-03-24 16:45:31 -0400

    adds URISyntaxException to methods using new way to get resource path

--- src/test/java/org/reactome/release/dataexport/utilities/EuropePMCFileUploaderTestUtils.java
+++ src/test/java/org/reactome/release/dataexport/utilities/EuropePMCFileUploaderTestUtils.java
@@ -4,30 +4,35 @@ import static org.reactome.release.dataexport.utilities.FTPFileUploaderTestUtils
 import static org.reactome.release.dataexport.utilities.FTPFileUploaderTestUtils.getPreviousReactomeReleaseNumber;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
 public class EuropePMCFileUploaderTestUtils {
-	public static List<String> getCurrentEuropePMCFilePathsInMockOutputDirectory() throws IOException {
+	public static List<String> getCurrentEuropePMCFilePathsInMockOutputDirectory()
+		throws IOException, URISyntaxException {
+
 		return FTPFileUploaderTestUtils.getPathsForCurrentFilesInMockOutputDirectory("europe_pmc");
 	}
 
-	public static List<String> getPreviousEuropePMCFilePathsInMockOutputDirectory() throws IOException {
+	public static List<String> getPreviousEuropePMCFilePathsInMockOutputDirectory()
+		throws IOException, URISyntaxException {
+
 		return FTPFileUploaderTestUtils.getPathsForPreviousFilesInMockOutputDirectory("europe_pmc");
 	}
 
-	public static String getCurrentEuropePMCProfileFileName() throws IOException {
+	public static String getCurrentEuropePMCProfileFileName() throws IOException, URISyntaxException {
 		return "europe_pmc_profile_reactome_" + getCurrentReactomeReleaseNumber() + ".xml";
 	}
 
-	public static String getPreviousEuropePMCProfileFileName() throws IOException {
+	public static String getPreviousEuropePMCProfileFileName() throws IOException, URISyntaxException {
 		return "europe_pmc_profile_reactome_" + getPreviousReactomeReleaseNumber() + ".xml";
 	}
 
-	public static String getCurrentEuropePMCLinksFileName() throws IOException {
+	public static String getCurrentEuropePMCLinksFileName() throws IOException, URISyntaxException {
 		return "europe_pmc_links_reactome_" + getCurrentReactomeReleaseNumber() + ".xml";
 	}
 
-	public static String getPreviousEuropePMCLinksFileName() throws IOException {
+	public static String getPreviousEuropePMCLinksFileName() throws IOException, URISyntaxException {
 		return "europe_pmc_links_reactome_" + getPreviousReactomeReleaseNumber() + ".xml";
 	}
 }
